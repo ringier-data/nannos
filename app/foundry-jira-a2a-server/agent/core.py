eb8cb8de8c258eac2b2c0743233cc94c436a36e7
aartaria <andrea.artaria@ringier.ch>
2025-11-21 11:15:14 +0100

    fix: new foundry agent

--- app/foundry-jira-a2a-server/agent/core.py
+++ app/foundry-jira-a2a-server/agent/core.py
@@ -1,53 +1,15 @@
 import logging
 import os
 from collections.abc import AsyncIterable
-from typing import List, Literal, Optional
+from typing import Literal, Optional
 
 from a2a.types import Task, TaskState
 from foundry_sdk import AsyncFoundryClient as PlatformAsyncFoundryClient
-from foundry_sdk import Auth
-from foundry_sdk import ConfidentialClientAuth as PlatformSDKConfidentialClientAuth
-from foundry_sdk import Config as FoundryConfig
+from foundry_sdk import Auth, ConfidentialClientAuth
 from ringier_a2a_sdk.agent import BaseAgent
 from ringier_a2a_sdk.models import AgentStreamResponse, UserConfig
 
 logger = logging.getLogger(__name__)
-
-
-class ConfidentialClientAuth(PlatformSDKConfidentialClientAuth):
-    def __init__(
-        self,
-        client_id: str,
-        client_secret: str,
-        hostname: Optional[str] = None,
-        scopes: Optional[List[str]] = None,
-        should_refresh: bool = False,
-        *,
-        config: Optional[FoundryConfig] = None,
-    ) -> None:
-        self._default_scopes: List[str] = [
-            "api:ontologies-read",
-            "api:ontologies-write",
-            "api:use-ontologies-read",
-            "api:use-ontologies-write",
-            "api:functions-read",
-            "api:use-functions-read",
-        ]
-
-        # If scopes are provided and not empty, append to default scopes
-        # Otherwise, use None
-        final_scopes = None
-        if scopes:
-            final_scopes = list(set(self._default_scopes + scopes))
-
-        super().__init__(
-            client_id=client_id,
-            client_secret=client_secret,
-            hostname=hostname,
-            scopes=final_scopes,
-            should_refresh=should_refresh,
-            config=config,
-        )
 
 
 class AsyncFoundryClient:
@@ -115,17 +77,17 @@ class FoundryJiraTicketAgent(BaseAgent):
         """Create async Foundry client."""
         # foundry_sdk.UserTokenAuth(os.environ["FOUNDRY_TOKEN"])
         auth = ConfidentialClientAuth(
-            client_id="7d52d6a5d1f0fafe3bc4a4f7ded3c01d",
+            client_id="bda4dfae9127a3dd60ad5af6ce3cf4a0",
             client_secret=os.environ["FOUNDRY_CLIENT_SECRET"],
             hostname="https://blumen.palantirfoundry.de",
             should_refresh=True,
             scopes=[
                 "api:use-ontologies-read",
                 "api:use-ontologies-write",
-                "api:use-mediasets-read",
-                "api:use-mediasets-write",
                 "api:use-aip-agents-read",
                 "api:use-aip-agents-write",
+                "api:use-mediasets-read",
+                "api:use-mediasets-write",
             ],
         )
         return AsyncFoundryClient(
